21e3302a1ad7b8dcb8f5fbc663a351969450345a
ILya Turukin <tr.wolfer@gmail.com>
2018-01-15 13:16:38 +0700

    Added Firebase and @angular/material integrations.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AngularFireModule} from 'angularfire2';
 
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDP8jeTulRDQDku1U2Ml41At2ciHVK1j7A',
+  authDomain: 'jogging-app-f1e0e.firebaseapp.com',
+  databaseURL: 'https://jogging-app-f1e0e.firebaseio.com',
+  projectId: 'jogging-app-f1e0e',
+  storageBucket: '',
+  messagingSenderId: '204969847199'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
